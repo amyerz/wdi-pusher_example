4969d4809d2ed4bf04f9fd5f12bf56e3139f5d39
Amy Erz <wingmerz@gmail.com>
2013-08-12 10:55:06 -0700

    update display show on main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,13 +1,17 @@
 class MainController < ApplicationController
   def index
+    @messages = Message.order("created_at DESC")
   end
 
   def send_message
     @message = params[:message][:text]
 
+    Message.create(content: @message)
+
     Pusher['chat'].trigger('new_message', {
-    	message: @message
-    	})
+      message: @message
+    })
+
     render nothing: true
   end
 end
